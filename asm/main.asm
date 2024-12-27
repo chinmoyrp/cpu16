0cd4b67d94d9c3368478d452c2c644848bba0a1d
Chinmoy R. Pradhan <chinmoyrp65@gmail.com>
2024-12-27 22:40:55 +0530

    Implement mov instruction and add a fibonacci sequence generator

--- asm/main.asm
+++ asm/main.asm
@@ -18,13 +18,15 @@
 }
 
 #subruledef st_source {
-    {immediate} => immediate`5 @ 0b0
-    {rs: register} => 0b000 @ rs @ 0b1
+    {immediate} => immediate`4 @ 0b0
+    {rs: register} => 0b00 @ rs @ 0b1
 }
 
 #ruledef {
     ld {rd: register}, {src: ld_source} => src @ rd @ 0x1
-    st mem[{address}], {src: st_source} => src @ address`6 @ 0x2
+    st mem[{address}], {src: st_source} => src @ address`6 @ 0`1 @ 0x2
+    st mem[{rs :register}], {src: st_source} => src @ 0`4 @ rs @ 1`1 @ 0x2
+	mov {rd: register}, {rs: register} =>  0`8 @ rs @ rd @ 0x3
     add {rd: register}, {immediate} => immediate`10 @ rd @ 0x4
     addr {rd: register}, {rs: register} => 0`8 @ rs @ rd @ 0x5
     sub {rd: register}, {immediate} => immediate`10 @ rd @ 0x6
@@ -38,3 +40,22 @@
     cmp {a: register}, {immediate} => immediate`10 @ a @ 0xE
     halt => 0xF`16
 }
+
+
+fibonacci:
+	ld r0, 1
+	ld r1, 1
+	st mem[0x20], r0
+	st mem[0x21], r1
+	ld r2, 0
+	ld r3, 1
+	.loop:
+		addr r0, r1
+		mov r3, r0
+		st mem[0x23], r3
+		mov r0, r1
+		mov r1, r3
+		add r2, 1
+		cmp r2, 5
+		jlt .loop
+	halt
